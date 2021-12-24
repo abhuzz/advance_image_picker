c806dcdfc0d43dc3c1e5ba58e2fb6b25b25d32dd
Paresh <paresh.oct7@gmail.com>
2021-12-24 12:15:51 +0530

    Fix gallery going back

--- lib/widgets/picker/video_picker.dart
+++ lib/widgets/picker/video_picker.dart
@@ -351,7 +351,7 @@ class _VideoPickerState extends State<VideoPicker>
               album: _currentAlbum!,
               onImageSelected: (image) async {
                 LogUtils.log("[_buildAlbumPreview] onImageSelected start");
-                Navigator.pop(context);
+                Navigator.pop(context, image.originalPath);
               })
               : const SizedBox(),
         )
